7ae25a5e29678df00186bb9647a02471793ffd84
ThomasSpare <t.spare.jkpg@gmail.com>
2024-07-10 20:56:13 +0200

    Pre deploy settings

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,14 @@ app.use(cors({ origin: "https://maste-science.onrender.com" }));
 
 app.use(express.json());
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../frontend/build/index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("Welcome to the MÅSTE project Database !");
 });
