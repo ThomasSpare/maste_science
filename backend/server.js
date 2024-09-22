ee4d0cd90b49db5cd0567fd30060fcdf82f5b71f
ThomasSpare <t.spare.jkpg@gmail.com>
2024-09-22 14:59:04 +0200

    Change to ../frontend/build in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,12 @@ const app = express();
 const port = process.env.PORT || 10000;
 
 // Serve static files from the React app -------------------------------
-app.use(express.static(path.join(__dirname, "frontend/build")));
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back index.html
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
 });
 //----------------------------------------------------------------------
 
