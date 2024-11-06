bb5c88b729b9aa20b78831ac4745a61d3d43c2de
ThomasSpare <t.spare.jkpg@gmail.com>
2024-11-06 22:41:36 +0100

    News item CSS styling

--- frontend/src/Auth/useAuth.js
+++ frontend/src/Auth/useAuth.js
@@ -10,14 +10,10 @@ export const useAuth = () => {
   } = useAuth0();
 
   const getAccessToken = async () => {
-    console.log("getAccessToken called"); // Log to verify function call
     try {
       const audience = process.env.REACT_APP_AUTH0_AUDIENCE;
       const scope =
         "read:users update:users delete:users create:users read:roles create:roles delete:roles update:roles"; // Add other scopes as needed
-      console.log("Audience:", audience);
-      console.log("Scope:", scope);
-
       const token = await getAccessTokenSilently({
         audience,
         scope,
